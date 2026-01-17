9dffcbb09ba39428b80f525f09a3936ba79eb682
harin1124 <lolrin1124@gmail.com>
2026-01-18 02:07:07 +0900

    feat: 인증 관련 에러 코드 추가

--- src/main/java/org/bplte/core/api/core/message/ResponseCodeGeneral.java
+++ src/main/java/org/bplte/core/api/core/message/ResponseCodeGeneral.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseCodeGeneral implements ResponseCodeInterface {
 	SUCCESS("BPLTE200", "정상 처리되었습니다.", ""),
-	BAD_REQUEST("BPLTE400", "잘못된 요청입니다.", "");
+	BAD_REQUEST("BPLTE400", "잘못된 요청입니다.", ""),
+	UNAUTHORIZED("BPLTE401", "인증이 필요합니다.", "로그인 후 다시 시도해주세요.");
 	
 	private final String resultCode;
 	private final String resultMessage;
