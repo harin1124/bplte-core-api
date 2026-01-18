6d3ea6e7237879d38b592429163f096c8322b489
harin1124 <lolrin1124@gmail.com>
2026-01-18 23:25:02 +0900

    feat: API 요청 시, 인증 토큰에서 사용자 아이디 추출하여 가져올 수 있도록 어노테이션 개발

---
+++ src/main/java/org/bplte/core/api/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.bplte.core.api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.bplte.core.api.config.resolver.CurrentUserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+	private final CurrentUserArgumentResolver currentUserArgumentResolver;
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(currentUserArgumentResolver);
+	}
+}
