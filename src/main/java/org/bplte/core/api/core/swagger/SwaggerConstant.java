29ee330eb6b05b73194c1acc622343a1c619626c
harin1124 <lolrin1124@gmail.com>
2026-02-11 12:18:18 +0900

    feat: Swagger 관련 값들 Constant로 분리

---
+++ src/main/java/org/bplte/core/api/core/swagger/SwaggerConstant.java
@@ -0,0 +1,6 @@
+package org.bplte.core.api.core.swagger;
+
+public class SwaggerConstant {
+	public static final String DATE_DEFAULT = "%Y-%m-%d %H:%i:%s";
+	public static final String DATE_EXAMPLE = "2026-10-02 11:15:00";
+}
