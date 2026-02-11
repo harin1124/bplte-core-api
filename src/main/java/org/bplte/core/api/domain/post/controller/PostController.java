4c26e928729e956041252159344da23bd34736e5
harin1124 <lolrin1124@gmail.com>
2026-02-11 12:28:34 +0900

    refactor: 포스트 컨트롤러 내 Swagger 설정 (Tag, Operation)

--- src/main/java/org/bplte/core/api/domain/post/controller/PostController.java
+++ src/main/java/org/bplte/core/api/domain/post/controller/PostController.java
@@ -1,10 +1,13 @@
 package org.bplte.core.api.domain.post.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.bplte.core.api.config.annotation.CurrentUser;
 import org.bplte.core.api.core.ApiResponse;
 import org.bplte.core.api.domain.post.dto.request.PostCreateRequest;
+import org.bplte.core.api.domain.post.dto.request.PostListRequest;
 import org.bplte.core.api.domain.post.dto.response.PostListResponse;
 import org.bplte.core.api.domain.post.service.PostService;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +17,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
+@Tag(name = "포스트 (PostController)")
 public class PostController {
 	private final PostService postService;
 	
 	@GetMapping
-	public ApiResponse<List<PostListResponse>> getPosts() {
-		return ApiResponse.success(postService.getPosts());
+	@Operation(summary = "포스트 목록 조회")
+	public ApiResponse<List<PostListResponse>> getPosts(@ModelAttribute PostListRequest request) {
+		return ApiResponse.success(postService.getPosts(request));
 	}
 	
 	@PostMapping
+	@Operation(summary = "포스트 등록")
 	public ApiResponse<Integer> createPost(
 			@RequestBody @Valid PostCreateRequest request,
 			@CurrentUser String userId) {
