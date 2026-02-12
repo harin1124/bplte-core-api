0ca94f4e2fa211334a5e6761afaca5b6f34ec63a
harin1124 <lolrin1124@gmail.com>
2026-02-12 15:06:24 +0900

    feat: 포스트 목록 조회 시 페이징을 위한 객체 생성 및 확장

---
+++ src/main/java/org/bplte/core/api/core/dto/request/PaginationRequest.java
@@ -0,0 +1,29 @@
+package org.bplte.core.api.core.dto.request;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PaginationRequest {
+	@Schema(description = "현재 페이지", defaultValue = "1")
+	private int page;
+	@Max(value = 1000)
+	@Schema(description = "페이지 개당 데이터 수", defaultValue = "10")
+	private int size;
+	@Hidden
+	private int limit;
+	@Hidden
+	private int offset;
+	
+	public int getLimit() {
+		return this.size;
+	}
+	
+	public int getOffset() {
+		return (this.page - 1) * this.size;
+	}
+}
---
+++ src/main/java/org/bplte/core/api/domain/post/dto/request/PostListRequest.java
@@ -0,0 +1,13 @@
+package org.bplte.core.api.domain.post.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bplte.core.api.core.dto.request.PaginationRequest;
+
+/**
+ * [요청] 포스트 조회
+ */
+@Getter
+@Setter
+public class PostListRequest extends PaginationRequest {
+}
