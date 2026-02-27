cc831828f13fe8bdfc0bc699233ee5e359f01a78
harin1124 <lolrin1124@gmail.com>
2026-02-27 14:35:36 +0900

    refactor: Swagger에 보이지 않도록 hidden 처리
    - 자동으로 토큰을 가져와서 사용자 아이디를 담아두는 형태로, Swagger에서 입력하는 것은 부적절하므로 조치 처리

--- src/main/java/org/bplte/core/api/config/annotation/CurrentUser.java
+++ src/main/java/org/bplte/core/api/config/annotation/CurrentUser.java
@@ -1,5 +1,7 @@
 package org.bplte.core.api.config.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,6 +10,7 @@ import java.lang.annotation.Target;
 /**
  * 현재 인증된 사용자의 아이디를 주입받기 위한 어노테이션
  */
+@Parameter(hidden = true)
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CurrentUser {
