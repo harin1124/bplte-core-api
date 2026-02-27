9e1097eafcbf9a21324475459cd9fea4546d2e61
harin1124 <lolrin1124@gmail.com>
2026-02-27 14:38:18 +0900

    refactor: 목록 조회 시, 오류를 유발시키는 잘못된 값을 넣을 수 없도록 처리

--- src/main/java/org/bplte/core/api/core/dto/request/PaginationRequest.java
+++ src/main/java/org/bplte/core/api/core/dto/request/PaginationRequest.java
@@ -3,15 +3,18 @@ package org.bplte.core.api.core.dto.request;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PaginationRequest {
+	@Min(1)
 	@Schema(description = "현재 페이지", defaultValue = "1")
 	private int page;
-	@Max(value = 1000)
+	@Min(1)
+	@Max(1000)
 	@Schema(description = "페이지 개당 데이터 수", defaultValue = "10")
 	private int size;
 	@Hidden
