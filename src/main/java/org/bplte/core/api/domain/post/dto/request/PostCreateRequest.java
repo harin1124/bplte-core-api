631207efae6d2eced753926632281c6ac89ac120
harin1124 <lolrin1124@gmail.com>
2026-02-27 14:38:55 +0900

    refactor: 유효성 로직 및 Swagger에 표시할 정보 데이터 추가

--- src/main/java/org/bplte/core/api/domain/post/dto/request/PostCreateRequest.java
+++ src/main/java/org/bplte/core/api/domain/post/dto/request/PostCreateRequest.java
@@ -1,6 +1,8 @@
 package org.bplte.core.api.domain.post.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,17 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "[요청] 포스트 등록 (PostCreateRequest)")
 public class PostCreateRequest {
 	@NotBlank
+	@Size(min = 1, max = 100)
+	@Schema(description = "제목", minLength = 1, maxLength = 100)
 	private String title;
+	@NotBlank
+	@Schema(description = "내용", minLength = 1)
 	private String content;
+	@Schema(hidden = true)
 	private String searchContent;
+	@Schema(hidden = true)
 	private String requestUserId;
 }
